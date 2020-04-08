173092f232adf7eb415384f09fcca91e3ea7c608
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2020-04-08 22:44:53 +0300

    Warehouse item model and register with Django Admin

--- app/brew_warehouse/admin.py
+++ app/brew_warehouse/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WarehouseItem
 
-# Register your models here.
+@admin.register(WarehouseItem)
+class WarehouseItemAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'pk', 'title', 'content', 'quantity'
+    )
