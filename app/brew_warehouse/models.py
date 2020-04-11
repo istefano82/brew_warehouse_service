d60610d6da6546d2e06d76b65a51cfb874b6565b
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2020-04-11 16:55:28 +0300

    Refactor PEP8

--- app/brew_warehouse/models.py
+++ app/brew_warehouse/models.py
@@ -1,9 +1,10 @@
-
 from django.db import models
+
 
 class WarehouseItem(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     quantity = models.IntegerField()
+
     def __str__(self):
         return self.title
--- app/brew_warehouse/serializers.py
+++ app/brew_warehouse/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+
 from .models import WarehouseItem
 
-class WarehouseItemSerializer(serializers.ModelSerializer):
 
+class WarehouseItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = WarehouseItem
         fields = '__all__'
